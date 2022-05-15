7826df3d25216e029287c171f13a37d0e8ac665b
Erich Ess <erich.g.ess@gmail.com>
2022-05-15 15:26:36 -0400

    Code clean up

--- thorns/src/bin/diff.rs
+++ thorns/src/bin/diff.rs
@@ -8,16 +8,19 @@ use thorns::trace::Trace;
 fn main() {
     let stage = "parser";
 
+    let left = "./data/diff/left";
+    let right = "./data/diff/right";
+
     // Open left Trace directory and associated code
-    let left_trace = open_trace("./data/diff/left").unwrap();
-    let left_sourcemap = open_sourcemap("./data/diff/left").unwrap();
+    let left_trace = open_trace(left).unwrap();
+    let left_sourcemap = open_sourcemap(left).unwrap();
 
     // Generate the graph for the left side
     let left_graph = get_graph(&left_trace, stage).unwrap();
 
     // Open right Trace directory and associated code
-    let right_trace = open_trace("./data/diff/right").unwrap();
-    let right_sourcemap = open_sourcemap("./data/diff/right").unwrap();
+    let right_trace = open_trace(right).unwrap();
+    let right_sourcemap = open_sourcemap(right).unwrap();
 
     // Generate the graph for the right side
     let right_graph = get_graph(&right_trace, stage).unwrap();
